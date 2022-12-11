0a79762bb5ce5bd090459d2b2edbe774e09b796a
JuanCruzAGB <43325751+JuanCruzAGB@users.noreply.github.com>
2022-12-11 16:04:11 -0300

    Instalación de Tailwind y Vue

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport"
+            content="width=device-width, initial-scale=1">
+
+        <title>
+            {{ config('app.name') }}
+        </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+            rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}"
+            rel="stylesheet">
+
+        <link href="{{ asset('css/styles.css') }}"
+            rel="stylesheet">
+    </head>
+
+    <body>
+        <main id="app"></main>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
